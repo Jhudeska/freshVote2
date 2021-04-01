2e3ba8df91348db91e83789e887ff78e6bb95146
Jhudeska <jvsignacia@live.nl>
2021-04-01 22:00:15 +0200

    Initial commit

---
+++ src/main/java/com/example/freshVote2/FreshVote2Application.java
@@ -0,0 +1,13 @@
+package com.example.freshVote2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshVote2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshVote2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/freshVote2/FreshVote2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freshVote2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshVote2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
